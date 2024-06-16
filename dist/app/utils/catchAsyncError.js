719072aab07378aec9a3e3a262e6d1fbf327a0da
minhaz1010 <minhazrahman1010@gmail.com>
2024-06-16 19:03:12 +0600

    add live link

--- dist/app/utils/catchAsyncError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const catchAsyncErrors = (fn) => {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch((error) => next(error));
-    };
-};
-exports.default = catchAsyncErrors;
--- dist/app/utils/userRole.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRole = void 0;
-exports.userRole = {
-    user: "user",
-    admin: "admin",
-};

d8e9edbd71f7d22c716a290f153bc17449d84b9d
minhaz1010 <minhazrahman1010@gmail.com>
2024-08-30 15:17:02 +0600

    add a logger (morgan)

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import cors from "cors";
 import { mainRoutes } from "./app/route";
 import notFound from "./app/middleware/notFound";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
-
+import morgan from "morgan";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello dear");
@@ -17,5 +18,5 @@ app.use("/api", mainRoutes);
 
 app.use(globalErrorHandler);
 
-app.use(notFound)
+app.use(notFound);
 export default app;

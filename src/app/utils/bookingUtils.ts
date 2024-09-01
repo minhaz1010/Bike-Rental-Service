237d8e5c89164ee895124507d94cc269a896f50b
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-01 13:12:09 +0600

    add finalpayment utils in bookingutils

--- src/app/utils/bookingUtils.ts
+++ src/app/utils/bookingUtils.ts
@@ -40,6 +40,37 @@ export const initiatePayment = async (paymentData: TSendinfo) => {
     throw new Error("Payment initiation failed!");
   }
 };
+export const finalPayment = async (paymentData: TSendinfo) => {
+  try {
+    const response = await axios.post(config.AMARPAY_API!, {
+      store_id: config.STORE_ID,
+      signature_key: config.SIGNATURE_KEY,
+      tran_id: paymentData.transactionId,
+      success_url: `http://localhost:3000/api/payment/final-confirmation?transactionId=${paymentData.transactionId}&status=success`,
+      fail_url: `http://localhost:3000/api/payment/confirmation?status=failed`,
+      cancel_url: "http://localhost:5173/",
+      amount: paymentData.totalPrice - 100,
+      currency: "BDT",
+      desc: "Merchant Registration Payment",
+      cus_name: paymentData.customerName,
+      cus_email: paymentData.customerEmail,
+      cus_add1: paymentData.customerAddress,
+      cus_add2: "N/A",
+      cus_city: "N/A",
+      cus_state: "N/A",
+      cus_postcode: "N/A",
+      cus_country: "N/A",
+      cus_phone: paymentData.customerPhone,
+      type: "json",
+    });
+
+    //console.log(response);
+    // * i will get payment url from response.data
+    return response.data;
+  } catch (err) {
+    throw new Error("Payment initiation failed!");
+  }
+};
 
 export const verifyPayment = async (tnxId: string) => {
   try {
